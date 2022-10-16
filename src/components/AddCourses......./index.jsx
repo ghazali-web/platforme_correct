f1db2d2032a62ffc5ad65d111c97b6771405f60a
ghazali-web <dejahichem96@gmail.com>
2022-10-16 12:52:12 +0100

    linking

--- src/components/AddCourses......./index.jsx
+++ src/components/AddCourses......./index.jsx
@@ -1,36 +1,89 @@
-import React from "react";
-import { useEffect, useState } from "react";
-const [course, setcouse] = useState([]);
-useEffect(() => {
-  fetch("/FormCourses")
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then((resjson) => {
-      setcouse(resjson);
-    });
-});
-
+import React, { useEffect } from "react";
 import "./style.css";
+import { useState } from "react";
+import { METHODS } from "http";
+import { Paper } from "@mui/material";
+
+// -------this is for getting from db ---------
+const [getcourse,setgetcourse]=useState([]);
+useEffect(()=>{
+  fetch("").then(res=>res.json()).then(result=>setgetcourse(result);
+},[])
+
+
+// useEffect(() => {
+//   fetch("/FormCourses")
+//     .then((res) => {
+//       if (res.ok) {
+//         return res.json();
+//       }
+//     })
+//     .then((resjson) => {
+//       setcouse(resjson);
+//     });
+// });
+//  {course.map((fromcourse) => {
+//         <p>{fromcourse}</p>;
+//       })}
+
+//  <TextField
+//         id=""
+//         label=""
+//         variant=""
+//         value=""
+//         onChange={(e) => setcouse(e.target.value)}
+//       /> 
+
+// -----------------this is for posting ------------------
+const [Dates, setDates] = useState("");
+const [Files, setFiles] = useState("");
+const handleclick = (e) => {
+  e.preventDefault();
+  const course = [Dates, Files];
+  fetch("", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(course),
+  }).then(() => {
+    console.log("New Student added");
+  });
+};
+
 const index = () => {
   return (
-    <div className="borderCourse">
-      {course.map((fromcourse) => {
-        <p>{fromcourse}</p>;
-      })}
-      <TextField id="" label="" variant="" />
-      <script>
+
+
+<div className="borderCourse">
+      
+{getcourse.map((result,i)=>{
+  <Paper key={i}>Id:{getcourse.id} <br />
+  date:{getcourse.Date} <br />
+  file:{getcourse.File} <br />
+  </Paper>
+})}
+      
+      
+      
+      
+      <script value={Dates} onChange={(e) => setDates(e.target.value)}>
         date = new Date().toLocaleDateString(); document.write(date);
       </script>
       <form>
         <label htmlFor="myfile">Select files:</label>
-        <input type="file" id="myfile" name="myfile" multiple />
-        <input type="submit" />
+        <input
+          value={Files}
+          onChange={(e) => setFiles(e.target.value)}
+          type="file"
+          id="myfile"
+          name="myfile"
+          multiple="true"
+        />
+        {/* <input  type="submit" /> */}
+        <button variant="contained" color="secondary" onClick={handleclick}>
+          Submit
+        </button>
       </form>
     </div>
   );
 };
-
 export default index;
